cc7bbc9738936e757fecf53eb2981b5127d31405
rhovian <atomicjab@proton.me>
2023-12-17 21:41:31 -0600

    add typing

--- packages/evm/hardhat.config.ts
+++ packages/evm/hardhat.config.ts
@@ -3,7 +3,7 @@ import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.23",
+    version: '0.8.23',
     settings: {
       optimizer: {
         enabled: true,
--- packages/evm/test/Poidh.ts
+++ packages/evm/test/Poidh.ts
@@ -1,28 +1,32 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import { Contract, ContractFactory } from "ethers";
 import { ethers } from "hardhat";
 import { expect } from "chai";
 
 describe("PoidhV2", function () {
 
-  let poidhV2: any;
-  let owner: any;
+  let poidhV2: Contract;
+  let poidhV2Factory: ContractFactory;
 
+  let owner: SignerWithAddress;
 
   beforeEach(async function () {
 
     [owner] = await ethers.getSigners();
 
-    poidhV2 = await ethers.getContractFactory("PoidhV2");
-    poidhV2 = await poidhV2.deploy(
-      owner.address, 
+    poidhV2Factory = await ethers.getContractFactory("PoidhV2");
+    poidhV2 = await poidhV2Factory.deploy(
+      owner.address,
       1000 // fee numerator
-    );
+    ) as Contract;
 
   });
 
   describe("Deployment", function () {
+
     it("Sets the right owner", async function() {
       expect(await poidhV2.treasury()).to.equal(owner.address);
     });
+
   });
 });
